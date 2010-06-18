20c817d1a3e5eecbadee1dc173516cb3d9ad6d50
Creative Dutchmen <hkeemink@gmail.com>
2010-06-18 23:48:25 +0200

    Removed debug data from index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-	xdebug_start_code_coverage();
-
 	define('DOCROOT', rtrim(dirname(__FILE__), '\\/'));
 	define('DOMAIN', rtrim(rtrim($_SERVER['HTTP_HOST'], '\\/') . dirname($_SERVER['PHP_SELF']), '\\/'));
 
